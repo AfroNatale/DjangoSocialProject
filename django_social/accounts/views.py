48e54ef6bb5467a7fae47517aef60a740d7bf219
Artemiusz Znojek <artemiusz.znojek@gmail.com>
2018-03-31 17:26:12 +0200

    User model and form

--- django_social/accounts/views.py
+++ django_social/accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import CreateView
+from . import forms
 
 # Create your views here.
+class Register(CreateView):
+    form_class = forms.UserCreateForm
+    success_url = reverse_lazy('login') #reverse_lazy execute until user hit submit button
+    template_name = 'accounts/register.html'
