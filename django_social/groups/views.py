7442b5a061f1ca865620110493c106b47dba144d
Artemiusz Znojek <artemiusz.znojek@gmail.com>
2018-04-11 23:03:18 +0200

    Join/leave group

--- django_social/groups/views.py
+++ django_social/groups/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import (LoginRequiredMixins,
                                         PermissionRequiredMixins)
+from django.contrib import messages
+
 from django.core.urlresolvers import reverse
 from django.views import generic
+from django.db import IntegrityError
+from django.shortcuts import get_object_or_404
 
 from groups.models import Group,GroupMember
+from . import models
 
 # Create your views here.
 class CreateGroup(LoginRequiredMixin, generic.CreateView):
@@ -16,3 +21,34 @@ class SingleGroup(generic.DetailView):
 
 class ListGroups(generic.ListView):
     model = Group
+
+class JoinGroup(LoginRequiredMixin, generic.RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse("groups:single", kwargs={"slug": self.kwargs.get("slug")})
+
+    def get(self, request, *args, **kwargs):
+        group = get_object_or_404(Group, slug=self.kwargs.get("slug"))
+        try:
+            GroupMember.objects.create(user=self.request.user, group=group)
+        except IntegrityError:
+            messages.warning(self.request, "Uwaga, jesteś już członkiem grupy {}".format(group.name)))
+        else:
+            messages.success(self.request, "Jesteś teraz członkiem grupy {}".format(group.name))
+
+        return super().get(request, *args, **kwargs)
+
+class LeaveGroup(LoginRequiredMixin, generic.RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse("groups:single",kwargs={"slug": self.kwargs.get("slug")})
+
+    def get(self, request, *args, **kwargs):
+        try:
+            membership = models.GroupMember.objects.filter(user=self.request.user, group__slug=self.kwargs.get("slug")).get()
+        except models.GroupMember.DoesNotExist:
+            messages.warning(self.request, "Nie możesz opuścić tej grupy, ponieważ do niej nie należysz.")
+        else:
+            membership.delete()
+            messages.success(self.request, "Pomyślnie opuściłeś grupę.")
+        return super().get(request, *args, **kwargs)
